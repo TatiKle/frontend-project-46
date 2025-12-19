dd75a9d13f54a26b42977d04059afc30e41179d6
TatiKle <136837744+TatiKle@users.noreply.github.com>
2025-12-19 22:25:22 +0300

    Add parse function for JSON and YAML formats

---
+++ src/parsers.js
@@ -0,0 +1,15 @@
+import yaml from 'js-yaml';
+
+const parse = (data, format) => {
+  switch (format) {
+    case 'json':
+      return JSON.parse(data);
+    case 'yaml':
+    case 'yml':
+      return yaml.load(data);
+    default:
+      throw new Error(`Unknown format: '${format}'`);
+  }
+};
+
+export default parse;
